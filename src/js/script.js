b6aac33be26cbf8361c0adcc9c9720758fbaf67a
CLUZEAU BENJAMIN <bcluzeau@iut.local>
2019-03-21 10:29:50 +0100

    waypoint

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+import 'waypoints/lib/noframework.waypoints.min.js'
+
+console.log(document.getElementById('retention'));
+
+var waypoint = new Waypoint({
+  element: document.getElementById('retention'),
+  handler: function(direction) {
+    document.getElementById('retention').classList.add('is-reached');
+  },
+  offset:'50%'
+});
